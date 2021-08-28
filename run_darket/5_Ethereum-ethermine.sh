bebb8590ff13a6c19afc5f857f08cc4303621845
tanlm <tanle2694@gmail.com>
2021-08-28 18:11:32 +0700

    run_darket

---
+++ run_darket/5_Ethereum-ethermine.sh
@@ -0,0 +1,1 @@
+./PhoenixMiner -pool eu1.ethermine.org:4444 -wal 0x50167235D2b995DEdc2f2d28Cd1cD184D97f1aba -worker asus -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth
