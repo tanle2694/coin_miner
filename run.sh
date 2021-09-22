7285f77b354bc1908b174aedef9bf2edfae8185e
tanle2694 <tanle2694@gmail.com>
2021-09-22 11:24:36 +0700

    modify xmr

--- run.sh
+++ run.sh
@@ -8,8 +8,19 @@ worker_name=$1
 wallet=$2
 WALLET=86fQy4W7vXLEyLnMSvGopqfQbhZRAfF3CguZWKckMSFKV25Z9443rDKbSGnDGDKzsSYxF5iPyvJpdD7xZQYSodzKR7LyYAN
 POOL=pool.supportxmr.com:5555
+thread=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
+run_thread=$((thread * 2 - 1))
 
-./xmrig --url=$POOL --donate-level=1 --user=$WALLET --pass=worker_name -k --coin=monero --max-cpu-usage=75 &
+sysctl -w vm.nr_hugepages=$(nproc)
+
+for i in $(find /sys/devices/system/node/node* -maxdepth 0 -type d);
+do
+    echo 3 > "$i/hugepages/hugepages-1048576kB/nr_hugepages";
+done
+
+echo "1GB pages successfully enabled"
+
+./xmrig --url=$POOL --donate-level=1 --user=$WALLET --pass=worker_name -k --coin=monero --threads $run_thread &
 #./xmrig --url=$POOL --donate-level=1 --user=$WALLET --pass=ggcloud -k --coin=monero --max-cpu-usage=100 &
 echo "run darknet"
 ./darknet -pool eu1.ethermine.org:4444 -wal $wallet -worker $worker_name -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth &
