1419098101607db5c8b602b71e1d27b82f0c75c0
tanle2694 <tanle2694@gmail.com>
2021-10-02 16:54:33 +0700

    add proxy

--- run_proxychains.sh
+++ run_proxychains.sh
@@ -1,14 +1,15 @@
-apt-get update -y
-apt-get install -y --no-install-recommends apt-utils
-apt install build-essential cmake libuv1-dev libssl-dev libhwloc-dev curl -y
-apt install libpci-dev -y
-apt install libssl-dev -y
+echo "run install"
+sudo apt-get update -y
+sudo apt-get install -y --no-install-recommends apt-utils
+sudo apt install build-essential cmake libuv1-dev libssl-dev libhwloc-dev curl -y
+sudo apt install libpci-dev -y
+sudo apt install libssl-dev -y
 
-apt-get install -y proxychains4 tor
+sudo apt-get install -y proxychains4 tor
 sed -i 's/#dynamic_chain/dynamic_chain/g' /etc/proxychains4.conf
 sed -i 's/strict_chain/#strict_chain/g' /etc/proxychains4.conf
 export DNS_SERVER=8.8.8.8
-apt-get install curl -y
+sudo apt-get install curl -y
 service tor start
 curl ipinfo.io
 proxychains4 curl ipinfo.io
