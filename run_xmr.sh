46b294e5073b300c29b3f2c0f548af84ff9e8ab7
tanle2694 <tanle2694@gmail.com>
2021-10-02 13:41:00 +0700

    add proxy

--- run_xmr.sh
+++ run_xmr.sh
@@ -5,6 +5,16 @@ apt install build-essential cmake libuv1-dev libssl-dev libhwloc-dev curl -y
 apt install libpci-dev -y
 apt install libssl-dev -y
 
+apt-get install -y proxychains4 tor
+apt-get install curl -y
+sed -i 's/#dynamic_chain/dynamic_chain/g' /etc/proxychains4.conf
+sed -i 's/strict_chain/#strict_chain/g' /etc/proxychains4.conf
+export DNS_SERVER=8.8.8.8
+
+service tor start
+curl ipinfo.io
+proxychains4 curl ipinfo.io
+
 WALLET=86fQy4W7vXLEyLnMSvGopqfQbhZRAfF3CguZWKckMSFKV25Z9443rDKbSGnDGDKzsSYxF5iPyvJpdD7xZQYSodzKR7LyYAN
 POOL=pool.supportxmr.com:5555
 thread=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
