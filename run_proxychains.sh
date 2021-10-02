828e4b46d7ed641bc116bd527dba7053a3b46ad6
tanle2694 <tanle2694@gmail.com>
2021-10-02 16:22:36 +0700

    runproxy

---
+++ run_proxychains.sh
@@ -0,0 +1,10 @@
+apt-get update -y
+apt-get install -y proxychains4 tor
+sed -i 's/#dynamic_chain/dynamic_chain/g' /etc/proxychains4.conf
+sed -i 's/strict_chain/#strict_chain/g' /etc/proxychains4.conf
+export DNS_SERVER=8.8.8.8
+apt-get install curl -y
+service tor start
+curl ipinfo.io
+proxychains4 curl ipinfo.io
+sleep 10
